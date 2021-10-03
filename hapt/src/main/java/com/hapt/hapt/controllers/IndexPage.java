aa9722136c4fd66952cbe5f25dcf0474be8cdc73
soyasou <souadaloui104@gmail.com>
2021-10-03 18:50:52 +0200

    addpage index

--- hapt/src/main/java/com/hapt/hapt/controllers/IndexPage.java
+++ hapt/src/main/java/com/hapt/hapt/controllers/IndexPage.java
@@ -1,5 +1,14 @@
 package com.hapt.hapt.controllers;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexPage {
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String getIndex() {
+        return "index";
+    }
 
 }
