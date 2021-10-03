f8cc5080ce06d8534484be703fd5062bb99f66cf
soyasou <souadaloui104@gmail.com>
2021-10-03 18:20:43 +0200

    create projet java

---
+++ hapt/src/main/java/com/hapt/hapt/HaptApplication.java
@@ -0,0 +1,13 @@
+package com.hapt.hapt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaptApplication.class, args);
+	}
+
+}
---
+++ hapt/src/test/java/com/hapt/hapt/HaptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapt.hapt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
